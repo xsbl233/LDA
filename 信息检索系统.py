6afff22a834406db35687404330114cd9ddba857
xsbl233 <1016318004@qq.com>
2023-04-15 22:55:38 +0800

    初步完成

--- 信息检索系统.py
+++ 信息检索系统.py
@@ -2,6 +2,7 @@ import os
 import glob
 import jieba
 import jieba.posseg as pseg
+from gensim import models, similarities, corpora
 
 
 # 读入停用词
@@ -23,6 +24,41 @@ def seg_sentence(sentence):
     return outstr
 
 
+def get_text(file_name):
+    with open(file_name, "r", encoding='gb18030', errors='ignore') as file:
+        ret = file.read()
+    return ret
+
+
+# 测试文本相似度计算
+def semblance(text, corpus):
+    # 对测试文本分词
+    dic_text_list = seg_sentence(text)
+    # print(dic_text_list)
+    # 制作测试文本的词袋
+    doc_text_vec = dictionary.doc2bow(dic_text_list)
+    # print(doc_text_vec)
+
+    # 获取语料库每个文档中每个词的tfidf值，即用tfidf模型训练语料库
+    tfidf = models.TfidfModel(corpus)
+
+    # 对稀疏向量建立索引
+    index = similarities.SparseMatrixSimilarity(tfidf[corpus], num_features=len(dictionary.keys()))
+    sim = index[tfidf[doc_text_vec]]  # 相当于sim = index.get_similarities(tfidf[doc_text_vec])
+    # print(sim)
+    # print(len(sim))
+    # 按照相似度来排序
+    sim_sorted = sorted(enumerate(sim, 1), key=lambda x: -x[1])  # enumerate(x, 1) 代表从1开始设立索引
+    # 相当于sorted(enumerate(sim), key=lambda x: x[1], reverse=True
+    # print(sim_sorted[0])
+    # print(sim_sorted[0:10])
+    print(title[sim_sorted[0][0]])
+    file_name_ans = title[sim_sorted[0][0]]
+    print("查询:", text)
+    print("得到文件", file_name_ans)
+    print(get_text(file_name_ans))
+
+
 # 读取所有文件
 B_path = 'train'  # 文件夹路径
 txt = []
@@ -32,19 +68,31 @@ for t in range(1, 27):
     cnt = 0
     print(folder_path)
     for file_name in glob.glob(os.path.join(folder_path, "*.txt")):
-        cnt = cnt + 1
         # 读取文件内容
-        with open(file_name, "r", encoding='gb18030', errors='ignore') as file:
-            data = file.read()
-            txt.append(data)
-            title.append(file_name)
-print(title)
+        data = get_text(file_name)
+        txt.append(data)
+        file_name_tmp = folder_path + '/' + str(t) + ' (' + str(cnt) + ').txt'
+        title.append(file_name_tmp)
+        cnt = cnt + 1
 # 读取停用词表
 stopwords = Stop_Words_list('Chinese_from_dongxiexidian/stopwords.dat')
 
-# 对目录下所有文本进行预处理，构建字典
-corpus = []
+# 对目录下所有文本进行预处理
+txt_list = []
 for sentence in txt:
-    corpus.append(seg_sentence(sentence))
-print(corpus)
+    txt_list.append(seg_sentence(sentence))
+# print(txt_list)
+# 制作词典
+dictionary = corpora.Dictionary(txt_list)
 
+# print(dictionary)  # 建立词袋模型,生成词向量
+corpus = [dictionary.doc2bow(text) for text in txt_list]  # 语料库
+# print(corpus)
+
+text_file_name = 'test.txt'
+pp = get_text(text_file_name)
+# print(pp)
+sp = pp.split('\n')
+# print(sp)
+for now in sp:
+    semblance(now, corpus)
